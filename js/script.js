bdec922ad5000fc82183b70c795b03e33f70f001
Ritesh Sharma <riteshrsharma71@gmail.com>
2024-03-04 09:04:23 +0530

    js-updated

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 console.log("Jai Shree Ram");
-
+let currSong = new Audio();
 async function getSongs() {
   let a = await fetch("http://127.0.0.1:3000/songs/");
   let response = await a.text();
-  console.log(response);
+  // console.log(response);
+
   let div = document.createElement("div");
   div.innerHTML = response;
   let as = div.getElementsByTagName("a");
@@ -15,30 +16,44 @@ async function getSongs() {
       songs.push(element.href.split("/songs/")[1]);
     }
   }
-
   return songs;
 }
+const playMusic = (track)=>{
+  let audio = new Audio("/songs/" + track);
+  audio.play();
+  // currSong.currentSrc = "/songs/" + track;
+  // currSong.play();
+}
 async function main() {
+  
+
+
   // get  the list of all songs
   var songs = await getSongs();
-  console.log(songs);
 
+  // Show All the sonngs on Playlist
   let songUL = document.querySelector(".song-list").getElementsByTagName('ul')[0];
   for(let i = 0 ; i < songs.length ; ++i){
-    songUL.innerHTML = songUL.innerHTML +  `<li>${songs[i].replaceAll("%20" , " ")}</li>`;
+    songUL.innerHTML = songUL.innerHTML +  `<li><img src="img/music.svg" class="invert" alt="">
+          <div class="info">
+            <div>${songs[i].replaceAll("%20" , " ")}</div>
+            <div>Ritesh</div>
+          </div>
+          <div class="play-now">
+            <img src="img/play.svg" class="invert" alt="">
+          </div>
+      </li>`;
   }
-  // play this first song
-  var audio = new Audio(songs[0]);
-  // audio.play();
-  // let play = document.getElementById("play-btn");
-  // play.addEventListener("click" , ()=>{
-  // })
 
-  audio.addEventListener("loadeddata", () => {
-    //   let duration = audio.duration;
-    // The duration variable now holds the duration (in seconds) of the audio clip
-    console.log(audio.duration, audio.currentTime, audio.currentSrc);
-  });
+  // Attach an Event Listner to Each Song
+  Array.from(document.querySelector(".song-list").getElementsByTagName("li")).forEach(e =>{
+    e.addEventListener("click" , ()=>{
+      console.log(e.querySelector(".info").firstElementChild.innerHTML);
+      playMusic(e.querySelector(".info").firstElementChild.innerHTML);
+    })
+  })
+
+  
 }
 
 main();
